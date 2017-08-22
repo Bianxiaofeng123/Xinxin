d97939e325b454899510c4ab820f98ab8db11609
fengfeng <fengfeng@DESKTOP-OPLVIH2>
2017-08-22 15:05:46 +0800

    欣欣-练习项目整合优化

--- java-xinxin-main/src/main/java/com/xinxin/Application.java
+++ java-xinxin-main/src/main/java/com/xinxin/Application.java
@@ -1,41 +1,30 @@
 package com.xinxin;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.xinxin.Application;
 
 @SpringBootApplication
 @Configuration
-// @EnableAutoConfiguration
-// 自动扫描根包名为com.eht.y1da的包。
-@ComponentScan(basePackages = { "java.xinxin.main" }) // 此处添加的不同的包名（即根包名不是com.eht.y1da）的时候，添加相应的根包名即可。
+//@EnableAutoConfiguration
+//自动扫描根包名为com.eht.y1da的包。
+@ComponentScan(basePackages = { "com.xinxin" }) //此处添加的不同的包名（即根包名不是com.eht.y1da）的时候，添加相应的根包名即可。
 @Controller
-// @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60)
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60)
 public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
-
+	
 	public static void main(String[] args) {
-		log.debug("XINXIN项目启动");
+		log.debug("com.xinxin项目启动");
 		SpringApplication.run(Application.class, args);
 	}
-	/*@Bean
-	public MessageSource messageSource() {
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasename("classpath:messages");
-		return messageSource;
-	}*/
 }
