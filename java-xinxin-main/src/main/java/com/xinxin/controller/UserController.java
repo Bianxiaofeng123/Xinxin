c314f76579400b1927140ca5ce7e9aad46d0dbeb
fengfeng <fengfeng@DESKTOP-OPLVIH2>
2017-09-11 19:59:03 +0800

    update

--- java-xinxin-main/src/main/java/com/xinxin/controller/UserController.java
+++ java-xinxin-main/src/main/java/com/xinxin/controller/UserController.java
@@ -29,41 +29,15 @@ public class UserController implements Serializable{
 	
 	@Autowired
 	UserService u;
-	
-    /** 
-	 * 示例 ：查询用户列表
-	 * @author fengfeng
-	 * @params  
-	 * @return
-	 * @throws Exception
-	 */
-	/*@RequestMapping(value = "/list",method=RequestMethod.GET)
+	 
+	@RequestMapping(value = "/list",method=RequestMethod.GET)
 	public JsonResult getUserList(@RequestParam Map<String, Object> params, Pager pager) throws Exception{
 		log.info("--->>>params:"+params);
 		JsonResult jsonResult = new JsonResult();
-		// 校验必填参数
-		//StringBuffer errMsg = new StringBuffer();
-		//if (!Validation.checkBlank(params, errMsg, messageSource, "uopid", "ebid")) {
-		//		jsonResult.putFailed(errMsg.toString());
-		//		return jsonResult;
-		//}
-		//获取翻页信息
-		pager.setPagerNecessary(params, pager);
 		//调用service
 		List<Map<String, Object>> userList=new ArrayList<Map<String, Object>>();
-		userList=userService.getUserList(params);
+		userList=u.getUserList(params);
 		jsonResult.put(userList);
 		return jsonResult;
-	}*/
-   
-		@RequestMapping(value = "/list",method=RequestMethod.GET)
-		public JsonResult getUserList(@RequestParam Map<String, Object> params, Pager pager) throws Exception{
-			log.info("--->>>params:"+params);
-			JsonResult jsonResult = new JsonResult();
-			//调用service
-			List<Map<String, Object>> userList=new ArrayList<Map<String, Object>>();
-			userList=u.getUserList(params);
-			jsonResult.put(userList);
-			return jsonResult;
-		}
+	}
 }
