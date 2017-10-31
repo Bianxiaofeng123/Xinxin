90e87ed3eeaaeafec31f3779171dcc8cb1c4c5be
Administrator <Administrator@USERCHI-PKO692V>
2017-10-31 18:42:48 +0800

    fff

--- java-xinxin-main/src/main/java/com/xinxin/service/MSGService.java
+++ java-xinxin-main/src/main/java/com/xinxin/service/MSGService.java
@@ -1,5 +1,6 @@
 package com.xinxin.service;
 
+import com.alibaba.druid.support.logging.Log;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.dysmsapi.model.v20170525.QuerySendDetailsRequest;
@@ -12,6 +13,8 @@ import com.aliyuncs.http.FormatType;
 import com.aliyuncs.http.HttpResponse;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
+
+import groovy.util.logging.Slf4j;
 
 import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
@@ -30,13 +33,16 @@ public class MSGService {
 
 	// 产品名称:云通信短信API产品,开发者无需替换
 	static final String product = "Dysmsapi";
-	// 产品域名,开发者无需替换
+	// 产品域名,开发者无需替换 
 	static final String domain = "dysmsapi.aliyuncs.com";
 
 	// TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-	static final String accessKeyId = "yourAccessKeyId";
-	static final String accessKeySecret = "yourAccessKeySecret";
+	static final String accessKeyId = "LTAIB5BXxny1Rb8v";
+	static final String accessKeySecret = "kG41oXcY2KGjV2QRNsYarApNrDA9d6";
 
+	static final String SMS_01 = "SMS_107055188";
+	static final String SMS_02 = "SMS_106955212";
+	
 	public static SendSmsResponse sendSms() throws ClientException {
 
 		// 可自助调整超时时间
@@ -51,19 +57,19 @@ public class MSGService {
 		// 组装请求对象-具体描述见控制台-文档部分内容
 		SendSmsRequest request = new SendSmsRequest();
 		// 必填:待发送手机号
-		request.setPhoneNumbers("15000000000");
+		request.setPhoneNumbers("17621777559");
 		// 必填:短信签名-可在短信控制台中找到
-		request.setSignName("云通信");
+		request.setSignName("卞晓峰");
 		// 必填:短信模板-可在短信控制台中找到
-		request.setTemplateCode("SMS_1000000");
+		request.setTemplateCode(SMS_01);
 		// 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
-		request.setTemplateParam("{\"name\":\"Tom\", \"code\":\"123\"}");
+		request.setTemplateParam("{\"name\":\"Tom\", \"code\":\"1314\"}");
 
 		// 选填-上行短信扩展码(无特殊需求用户请忽略此字段)
 		// request.setSmsUpExtendCode("90997");
 
 		// 可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
-		request.setOutId("yourOutId");
+		request.setOutId("fengfeng");
 
 		// hint 此处可能会抛出异常，注意catch
 		SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
@@ -85,7 +91,7 @@ public class MSGService {
 		// 组装请求对象
 		QuerySendDetailsRequest request = new QuerySendDetailsRequest();
 		// 必填-号码
-		request.setPhoneNumber("15000000000");
+		request.setPhoneNumber("17621190543");
 		// 可选-流水号
 		request.setBizId(bizId);
 		// 必填-发送日期 支持30天内记录查询，格式yyyyMMdd
@@ -105,6 +111,7 @@ public class MSGService {
 	public static void main(String[] args) throws ClientException, InterruptedException {
 
 		// 发短信
+		System.out.println("短信发送开始的接口----------------");
 		SendSmsResponse response = sendSms();
 		System.out.println("短信接口返回的数据----------------");
 		System.out.println("Code=" + response.getCode());
