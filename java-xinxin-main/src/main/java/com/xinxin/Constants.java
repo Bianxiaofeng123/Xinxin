93c2d34612cbc96f83e62b70688d7338b3f9bbba
Administrator <Administrator@USERCHI-PKO692V>
2017-11-01 10:27:43 +0800

    updage

--- java-xinxin-main/src/main/java/com/xinxin/Constants.java
+++ java-xinxin-main/src/main/java/com/xinxin/Constants.java
@@ -6,5 +6,18 @@ import java.util.List;
 import java.util.Map;
 
 public class Constants {
-	
+	// 产品名称:云通信短信API产品,开发者无需替换
+	public static final String product = "Dysmsapi";
+	// 产品域名,开发者无需替换 
+	public static final String domain = "dysmsapi.aliyuncs.com";
+
+	// TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
+	public static final String accessKeyId = "LTAIB5BXxny1Rb8v";
+	public static final String accessKeySecret = "kG41oXcY2KGjV2QRNsYarApNrDA9d6";
+
+	//注册模版
+	public static final String SMS_REGISTER = "SMS_107055188";
+	//打招呼模版
+	public static final String SMS_HELLOW = "SMS_106955212";
+	public static final String SIGNATURE = "卞晓峰";
 }
--- java-xinxin-main/src/main/java/com/xinxin/service/MSGService.java
+++ java-xinxin-main/src/main/java/com/xinxin/service/MSGService.java
@@ -1,25 +1,21 @@
 package com.xinxin.service;
 
-import com.alibaba.druid.support.logging.Log;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.dysmsapi.model.v20170525.QuerySendDetailsRequest;
 import com.aliyuncs.dysmsapi.model.v20170525.QuerySendDetailsResponse;
 import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
 import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
-import com.aliyuncs.dysmsapi.transform.v20170525.SendSmsResponseUnmarshaller;
 import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.http.FormatType;
-import com.aliyuncs.http.HttpResponse;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
-
-import groovy.util.logging.Slf4j;
-
-import java.nio.charset.Charset;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.UUID;
+import com.google.gson.Gson;
+import com.xinxin.Constants;
 
 /**
  * 阿里云短信服务（发送端）
@@ -30,40 +26,29 @@ import java.util.UUID;
  * 备注:Demo工程编码采用UTF-8 国际短信发送请勿参照此DEMO
  */
 public class MSGService {
-
-	// 产品名称:云通信短信API产品,开发者无需替换
-	static final String product = "Dysmsapi";
-	// 产品域名,开发者无需替换 
-	static final String domain = "dysmsapi.aliyuncs.com";
-
-	// TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-	static final String accessKeyId = "LTAIB5BXxny1Rb8v";
-	static final String accessKeySecret = "kG41oXcY2KGjV2QRNsYarApNrDA9d6";
-
-	static final String SMS_01 = "SMS_107055188";
-	static final String SMS_02 = "SMS_106955212";
 	
-	public static SendSmsResponse sendSms() throws ClientException {
-
+	public static SendSmsResponse sendSms(String sms_code,String phone,Map<String, Object> Param) throws ClientException {
+		System.out.println(Constants.accessKeyId+":"+Constants.accessKeySecret);
 		// 可自助调整超时时间
 		System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
 		System.setProperty("sun.net.client.defaultReadTimeout", "10000");
 
 		// 初始化acsClient,暂不支持region化
-		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
-		DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", Constants.accessKeyId, Constants.accessKeySecret);
+		DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", Constants.product, Constants.domain);
 		IAcsClient acsClient = new DefaultAcsClient(profile);
 
 		// 组装请求对象-具体描述见控制台-文档部分内容
 		SendSmsRequest request = new SendSmsRequest();
 		// 必填:待发送手机号
-		request.setPhoneNumbers("17621777559");
+		request.setPhoneNumbers(phone);
 		// 必填:短信签名-可在短信控制台中找到
-		request.setSignName("卞晓峰");
+		request.setSignName(Constants.SIGNATURE);
 		// 必填:短信模板-可在短信控制台中找到
-		request.setTemplateCode(SMS_01);
+		request.setTemplateCode(sms_code);
 		// 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
-		request.setTemplateParam("{\"name\":\"Tom\", \"code\":\"1314\"}");
+		Gson gson=new Gson();
+		request.setTemplateParam(gson.toJson(Param));
 
 		// 选填-上行短信扩展码(无特殊需求用户请忽略此字段)
 		// request.setSmsUpExtendCode("90997");
@@ -84,8 +69,8 @@ public class MSGService {
 		System.setProperty("sun.net.client.defaultReadTimeout", "10000");
 
 		// 初始化acsClient,暂不支持region化
-		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
-		DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", Constants.accessKeyId, Constants.accessKeySecret);
+		DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", Constants.product, Constants.domain);
 		IAcsClient acsClient = new DefaultAcsClient(profile);
 
 		// 组装请求对象
@@ -112,7 +97,10 @@ public class MSGService {
 
 		// 发短信
 		System.out.println("短信发送开始的接口----------------");
-		SendSmsResponse response = sendSms();
+		Map<String, Object> paramsMap=new HashMap<String,Object>();
+		paramsMap.put("code", "123456");
+		paramsMap.put("name", "卞先生");
+		SendSmsResponse response = sendSms(Constants.SMS_REGISTER,"17621190543",paramsMap);
 		System.out.println("短信接口返回的数据----------------");
 		System.out.println("Code=" + response.getCode());
 		System.out.println("Message=" + response.getMessage());
